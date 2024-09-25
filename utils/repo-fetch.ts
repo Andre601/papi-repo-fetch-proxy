91a28162ab402789e19e7e0f6b0a50cf1469d24b
Andre_601 <github@andre601.ch>
2024-09-26 01:07:43 +0200

    Update repo-fetch.ts

--- utils/repo-fetch.ts
+++ utils/repo-fetch.ts
@@ -1,6 +1,6 @@
 import { $fetch } from "ofetch";
 export const $repo = $fetch.create({
-  baseURL: "https://repo.extendedclip.com/service/local/artifact/maven",
+  baseURL: "https://repo.extendedclip.com/api/maven",
   headers: {
     "User-Agent": "PlaceholderAPI Wiki Version Fetcher <https://github.com/PlaceholderAPI>",
     Accept: "application/json",
